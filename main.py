2bbff523a5b9a3095e1a395a5184d15b6ceab8c5
Abdul Kadhar <abdulkadhar.sulthanbasha@ticvic.com>
2024-06-12 02:18:55 +0530

    refinement

--- main.py
+++ main.py
@@ -13,11 +13,10 @@ def read_item(image_url: str, question: str):
     model = AutoModelForCausalLM.from_pretrained(model_id, device_map="cuda", trust_remote_code=True, torch_dtype="auto", _attn_implementation='flash_attention_2') # use _attn_implementation='eager' to disable flash attention
     processor = AutoProcessor.from_pretrained(model_id, trust_remote_code=True) 
     messages = [ 
-        {"role": "user", "content": "<|image_1|>\nWhat is shown in this image?"},
-        {"role": "user", "content": question}   
+        {"role": "user", "content": "<|image_1|>\nWhat is shown in this image?"},        
     ] 
     url = "https://assets-c4akfrf5b4d3f4b7.z01.azurefd.net/assets/2024/04/BMDataViz_661fb89f3845e.png" 
-    image = Image.open(requests.get(image_url, stream=True).raw) 
+    image = Image.open(requests.get(url, stream=True).raw) 
     prompt = processor.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
     inputs = processor(prompt, [image], return_tensors="pt").to("cuda:0") 
     generation_args = { 
